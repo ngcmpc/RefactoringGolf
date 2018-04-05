bce2bde0c086d8a6493276268aa26ec75a2092d0
ngcmpc <ngcmpc@gmail.com>
2018-04-05 10:29:26 +0100

    trial2

--- src/main/java/com/develogical/golf/MoveInstruction.java
+++ src/main/java/com/develogical/golf/MoveInstruction.java
@@ -2,8 +2,10 @@ package com.develogical.golf;
 
 public class MoveInstruction extends Instruction {
     private final int amount;
+	private final long x;
 
     public MoveInstruction(int amount) {
        this.amount = amount;
+	   this.x = 1L;
     }
 }
--- src/main/java/com/develogical/golf/TurnInstruction.java
+++ src/main/java/com/develogical/golf/TurnInstruction.java
@@ -2,8 +2,10 @@ package com.develogical.golf;
 
 public class TurnInstruction extends Instruction {
     private final int amount;
+	private final long x;
 
     public TurnInstruction(int amount) {
         this.amount = amount;
+		this.x = 1L;
     }
 }
