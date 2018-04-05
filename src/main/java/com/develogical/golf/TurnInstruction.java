ac236e98ced13e133d46ab16f5e8db035fe1eaaa
ngcmpc <ngcmpc@gmail.com>
2018-04-05 19:35:44 +0100

    no message

--- src/main/java/com/develogical/golf/TurnInstruction.java
+++ src/main/java/com/develogical/golf/TurnInstruction.java
@@ -2,10 +2,8 @@ package com.develogical.golf;
 
 public class TurnInstruction extends Instruction {
     private final int amount;
-	private final long x;
 
     public TurnInstruction(int amount) {
         this.amount = amount;
-		this.x = 1L;
     }
 }
