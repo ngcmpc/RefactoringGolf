99f4f62ed080aa857224aa330b8bb5b0486d7e52
ngcmpc <ngcmpc@gmail.com>
2018-04-05 19:36:11 +0100

    Revert "trial1 (2)"
    This reverts commit af378362c3f53b2252d28cbfa2c668f54d12fc71.

--- src/main/java/com/develogical/golf/MoveInstruction.java
+++ src/main/java/com/develogical/golf/MoveInstruction.java
@@ -2,10 +2,8 @@ package com.develogical.golf;
 
 public class MoveInstruction extends Instruction {
     private final int amount;
-	private final String name;
 
     public MoveInstruction(int amount) {
        this.amount = amount;
-	   this.name = "";
     }
 }
